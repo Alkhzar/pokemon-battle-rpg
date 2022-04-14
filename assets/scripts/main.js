a67810f91ad6f508aded2401affd8f2b8207dfc6
= <allankhza@gmail.com>
2022-04-13 21:59:28 -0500

    changed the pokemon randomizer to use PokeAPI instead to populate data

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,35 +1,3 @@
-// Create base pokemon constructor
-function Pokemon(name, level, hp, attackDamage, critRate, critMultiplier, healStrength, accuracy, frontImg, backImg) {
-    this.name = name;
-    this.level = level;
-    this.hp = hp;
-    this.attackDamage = attackDamage;
-    this.critRate = critRate;
-    this.critMultiplier = critMultiplier;
-    this.healStrength = healStrength;
-    this.accuracy = accuracy;
-    this.frontImg = frontImg;
-    this.backImg = backImg;
-}
-
-// Create 3 different pokemon from the constructor
-const bulbasaur = new Pokemon("Bulbasaur", 1, 100, 15, .2, 1.5, 10, .8, "assets/images/bulbasaurFront.png", "assets/images/bulbasaurBack.png");
-const charmander = new Pokemon("Charmander", 1, 100, 15, .2, 1.5, 10, .8, "assets/images/charmanderFront.png", "assets/images/charmanderBack.png");
-const squirtle = new Pokemon("Squirtle", 1, 100, 15, .2, 1.5, 10, .8, "assets/images/squirtleFront.png", "assets/images/squirtleBack.png");
-
-// Create an array with the 3 pokemon created
-const pokemonArr = [bulbasaur, charmander, squirtle];
-
-// Create a randomizer function
-function pokemonRandomizer(arr) {
-    const rand = Math.floor(Math.random() * pokemonArr.length);
-    return arr[rand];
-}
-
-// randomize enemy and player starting pokemon
-const enemyPokemon = pokemonRandomizer(pokemonArr);
-const playerPokemon = pokemonRandomizer(pokemonArr);
-
 // assign relevant ui to variables
 // enemy
 let enemyName = document.querySelector(".enemyName");
@@ -44,25 +12,84 @@ let playerHP = document.querySelector(".playerHPBar");
 let playerImage = document.querySelector(".playerImage");
 let playerDialogue = document.querySelector(".playerDialogue");
 
+// Create base pokemon constructor
+function Pokemon(name = "", hp = 0, attackDamage = 0, frontImg = "", backImg = "") {
+    this.name = name;
+    this.level = 1;
+    this.hp = hp;
+    this.attackDamage = attackDamage;
+    this.critRate = .2;
+    this.critMultiplier = 1.5;
+    this.healStrength = attackDamage;
+    this.accuracy = .8;
+    this.frontImg = frontImg;
+    this.backImg = backImg;
+}
+
+// create the 2 pokemon from the pokemon constructor
+const enemyPokemon = new Pokemon();
+const playerPokemon = new Pokemon();
 
 // create a ui update function
 function updateUI() {
     //enemy
     enemyName.innerText = enemyPokemon.name;
     enemyLevel.innerText = enemyPokemon.level;
+    enemyHP.max = enemyPokemon.hp;
     enemyHP.value = enemyPokemon.hp;
     enemyImage.src = enemyPokemon.frontImg;
     enemyDialogue.innerText = `Enemy's ${enemyPokemon.name} is ready to go!`;
     // player
     playerName.innerText = playerPokemon.name;
     playerLevel.innerText = playerPokemon.level;
+    playerHP.max = playerPokemon.hp;
     playerHP.value = playerPokemon.hp;
     playerImage.src = playerPokemon.backImg;
     playerDialogue.innerText = `Player's ${playerPokemon.name} is ready to go!`;
 }
 
-// update the ui on page load
-updateUI();
+// create constant variable holding the max pokemon ID
+const MAX_POKEMON_ID = 898;
+
+// create random num generator between 1 and MAX_POKEMON_ID
+const rand = () => Math.ceil(Math.random() * MAX_POKEMON_ID);
+
+// assign 2 pokemon variables to hold pokemon data
+let pokemon1 = pokemon2 = {};
+fetch(`https://pokeapi.co/api/v2/pokemon/${rand()}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            pokemon1 = data;
+            // fill in the enemyPokemon object
+            enemyPokemon.name = pokemon1.name;
+            enemyPokemon.hp = pokemon1.stats[0].base_stat;
+            enemyPokemon.attackDamage = Math.ceil(pokemon1.stats[1].base_stat / 7);
+            enemyPokemon.frontImg = pokemon1.sprites.front_default;
+            enemyPokemon.backImg = pokemon1.sprites.back_default || pokemon1.sprites.front_default;
+            enemyPokemon.healStrength = Math.ceil(pokemon1.stats[1].base_stat / 10);
+
+            // update the ui
+            updateUI();
+        })
+        .catch(err => console.log(err));
+fetch(`https://pokeapi.co/api/v2/pokemon/${rand()}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            pokemon2 = data;
+            // fill in the playerPokemon object
+            playerPokemon.name = pokemon2.name;
+            playerPokemon.hp = pokemon2.stats[0].base_stat;
+            playerPokemon.attackDamage = Math.ceil(pokemon2.stats[1].base_stat / 7);
+            playerPokemon.frontImg = pokemon2.sprites.front_default;
+            playerPokemon.backImg = pokemon2.sprites.back_default || pokemon2.sprites.front_default;
+            playerPokemon.healStrength = Math.ceil(pokemon2.stats[1].base_stat / 10);
+
+            // update the ui
+            updateUI();
+        })
+        .catch(err => console.log(err));
 
 // Add event listeners
 document.querySelector(".attack").addEventListener("click", attack);
